2f66678edffd0c15cfef9de2f40e8d57f876e7d8
YYriina <yoann.forthulman@gmail.com>
2019-03-07 07:04:28 +0100

    change router set up

--- routes/clientRouter.go
+++ routes/clientRouter.go
@@ -7,13 +7,16 @@ func clientRoutes() {
 	/*
 		serve static folder "static-client" with "/" for root path
 	*/
+
+	A.Static("/", "./clients/user/build")
+	A.Static("/public", "./static")
+
 	A.File("/", "./clients/user/build/index.html")
 	A.File("/about-me", "./clients/user/build/index.html")
 	A.File("/contact", "./clients/user/build/index.html")
 	A.File("/works", "./clients/user/build/index.html")
 	A.File("/service", "./clients/user/build/index.html")
-	A.Static("/blog", "./clients/user/build")
-	A.Static("/public", "./static")
+	A.File("/blog", "./clients/user/build/index.html")
 
 	/*
 		Define client pages and bind controller to them
