31eacba9959b37f362cb11eb2d61be90e4510a26
YYriina <yoann.forthulman@gmail.com>
2019-03-15 17:47:14 +0100

    fix router bug

--- routes/clientRouter.go
+++ routes/clientRouter.go
@@ -7,8 +7,11 @@ func clientRoutes() {
 	/*
 		serve static folder "static-client" with "/" for root path
 	*/
-
-	A.File("/*", "./clients/user/build/index.html")
-	A.Static("/", "./clients/user/build/")
+	A.Static("/*", "./clients/user/build")
+	A.Static("/about-me", "./clients/user/build")
+	A.Static("/contact", "./clients/user/build")
+	A.Static("/works", "./clients/user/build")
+	A.Static("/services", "./clients/user/build")
+	A.Static("/blog", "./clients/user/build")
 
 }
